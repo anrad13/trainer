80d72d5e4c5d21c1afe9b35b1da4a65236b4f82a
Andrey, Rad <anrad13@gmail.com>
2020-10-25 11:34:05 +0100

    Some improvement and code rearrange

--- src/main/java/org/example/GoogleTranslatorForm.java
+++ src/main/java/org/example/GoogleTranslatorForm.java
@@ -4,10 +4,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.interactions.Actions;
-
 import java.util.concurrent.TimeUnit;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.$;
@@ -61,6 +58,5 @@ public class GoogleTranslatorForm {
     public void sleep5() {
         sleep(5);
     }
-
 
 }
